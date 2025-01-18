bb2b4bfb233d6e46980867b24fcd07634fad7070
Shaini2003 <shinitharushika2020@gmail.com>
2025-01-19 00:55:07 +0530

    Add project files.

---
+++ Models/employees.cs
@@ -0,0 +1,12 @@
+﻿namespace cafe_management_system.Models
+{
+    public class employees
+    {
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string MiddleName { get; set; }
+        public string LastName { get; set; }
+        public string EmailAddress { get; set; }
+        public int PhoneNo { get; set; }
+    }
+}
